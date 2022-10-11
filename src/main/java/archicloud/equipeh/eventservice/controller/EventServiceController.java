29f2b95791defef2e002e2ce3bec4228da64a4e6
AMAIISDOM\herauda <antoine.heraud@amadeus.com>
2022-10-11 15:21:31 +0200

    More routes

--- src/main/java/archicloud/equipeh/eventservice/controller/EventServiceController.java
+++ src/main/java/archicloud/equipeh/eventservice/controller/EventServiceController.java
@@ -1,13 +1,27 @@
 package archicloud.equipeh.eventservice.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import archicloud.equipeh.eventservice.model.Event;
+import archicloud.equipeh.eventservice.service.EventService;
+
 @RestController
 @RequestMapping("/events")
 public class EventServiceController {
+	@Autowired
+	EventService eventService;
+	
+	@GetMapping
+	public ResponseEntity<List<Event>> getEvents() {
+		return ResponseEntity.ok(eventService.getEvents());
+	}
+	
 	@GetMapping(value = "/addEvent")
 	public ResponseEntity<String> addEvent() {
 		return ResponseEntity.ok("Add Event - OK");
