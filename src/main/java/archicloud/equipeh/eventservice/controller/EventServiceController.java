e15f44323207b2ff1dfbddf39c151980f17201df
AMAIISDOM\herauda <antoine.heraud@amadeus.com>
2022-10-11 15:06:54 +0200

    Routes

--- src/main/java/archicloud/equipeh/eventservice/controller/EventServiceController.java
+++ src/main/java/archicloud/equipeh/eventservice/controller/EventServiceController.java
@@ -8,8 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/events")
 public class EventServiceController {
-	@GetMapping(value = "/all")
-	public ResponseEntity<String> getModulesAccess() {
-		return ResponseEntity.ok("All Events");
+	@GetMapping(value = "/addEvent")
+	public ResponseEntity<String> addEvent() {
+		return ResponseEntity.ok("Add Event - OK");
+	}
+	
+	@GetMapping(value = "/updateEvent")
+	public ResponseEntity<String> updateEvent() {
+		return ResponseEntity.ok("Update Event - OK");
+	}
+	
+	@GetMapping(value = "/removeTicket")
+	public ResponseEntity<String> removeTicket() {
+		return ResponseEntity.ok("Remove Ticket - OK");
+	}
+	
+	@GetMapping(value = "/blockTicket")
+	public ResponseEntity<String> blockTicket() {
+		return ResponseEntity.ok("Block Ticket - OK");
+	}
+	
+	@GetMapping(value = "/unblockTicket")
+	public ResponseEntity<String> unblockTicket() {
+		return ResponseEntity.ok("Unblock Ticket - OK");
 	}
 }
